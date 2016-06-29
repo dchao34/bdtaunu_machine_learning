e606a02f7de4665c82358e87dd2bd210d89e7f46
yli5 <yli5@caltech.edu>
2016-06-29 11:26:39 -0700

    gbdt10 train

--- signal_detector/additive_models/model_evaluate.py
+++ signal_detector/additive_models/model_evaluate.py
@@ -64,7 +64,7 @@ if __name__ ==  '__main__':
     print 'plotting...'
     prob1 = evaluator.predict_proba(X_num, X_cat)[:,1]
     fpr,tpr,thresholds = roc_curve(y, prob1, sample_weight=w)
-    plt.plot(fpr,tpr,'r-',label='logistic')
+    plt.plot(fpr,tpr,'b-',label='logistic')
 
 
 
